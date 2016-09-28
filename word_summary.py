440437ee41a5f4396b38d840071db8fb264e1ca1
John Coppola <jdcoppola@gmail.com>
2016-09-28 14:10:49 -0400

    completes bonus challenge in word_summary

--- word_summary.py
+++ word_summary.py
@@ -1,3 +1,7 @@
+print "Write a program to read in the text of that file and count the number of\
+ occurrences of each word in that file. You will print one line for each word \
+ encountered, and output the number of times that word appearred in the file."
+
 # program prints out the frequency of each word in a file. two ways to
 # eliminate extra characters commented below:
 
@@ -12,8 +16,8 @@ for line in my_file_lines:
         word = word.replace('.', '').replace('(', '').replace(')', '')
         words_encountered[word] = words_encountered.get(word, 0) + 1
 
-for word, frequency in words_encountered.items():
-    print "%s appears %d times" % (word, frequency)
+# for word, frequency in words_encountered.items():
+#     print "%s appears %d times" % (word, frequency)
 
 
 ##########
@@ -33,8 +37,28 @@ words_encountered = {}
 for word in my_file_list:
     words_encountered[word] = words_encountered.get(word, 0) + 1
 
-for word, frequency in words_encountered.items():
-    print "%s appears %d times" % (word, frequency)
+# for word, frequency in words_encountered.items():
+#     print "%s appears %d times" % (word, frequency)
 
 
 # both ways produce the same result!
+
+
+### BONUS CHALLENGE: ###
+print "Print the top 10 most frequently used words in the file"
+entries = []
+
+# we want to make a list of tuples (right now we have words_encountered, which
+# is a dictionary of key-value pairs)
+entries = words_encountered.items()
+
+# print the sorted list with either sorted(), which doesn't affect the original
+# print sorted(entries, key=lambda tup: tup[1], reverse=True)
+
+# ... or with sort(), which does replace the original and returns "None"
+entries.sort(key=lambda tup: tup[1], reverse=True)
+# print entries
+
+# print the top ten most commonly-appearing words
+for i in range(10):
+    print entries[i]
